6f4086fcfd07e2b0f302212e40163a3ae9a5bf5b
Tarun Thomas <134177814+TarunThomas03@users.noreply.github.com>
2024-11-04 22:45:14 +0000

    final

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function openTab(tabName) {
     var i, tabcontent, tabbuttons;
+    
+    // Hide all tab contents
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";  
+        tabcontent[i].classList.remove("active");
     }
+
+    // Remove "active" class from all buttons
     tabbuttons = document.getElementsByClassName("btn");
     for (i = 0; i < tabbuttons.length; i++) {
         tabbuttons[i].classList.remove("active");
     }
-    document.getElementById(tabName).style.display = "block";  
+
+    // Show the selected tab and add "active" class to the corresponding button
+    document.getElementById(tabName).classList.add("active");
     document.querySelector(`button[onclick="openTab('${tabName}')"]`).classList.add("active");
 }
 
